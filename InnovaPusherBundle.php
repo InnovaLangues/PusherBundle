4577b491fb182c48986d5f53c01ed5eb07a52958
arnaudbey <arnaud.bey@gmail.com>
2015-11-25 10:13:22 +0100

    first commit.

---
+++ InnovaPusherBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Innova\PusherBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class InnovaPusherBundle extends Bundle
+{
+}
---
+++ Manager/PusherManager.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Innova\PusherBundle\Manager;
+
+class PusherManager
+{
+    protected $url;
+    protected $app;
+    protected $key;
+    protected $secret;
+
+    public function __construct($url, $app, $key, $secret)
+    {
+        $this->url = $url;
+        $this->app = $app;
+        $this->key = $key;
+        $this->secret = $secret;
+    }
+
+    public function checkCompatibility()
+    {
+        if (!extension_loaded('json')) {
+            throw new PusherException('There is a missing mandatory extension - please ensure that JSON is installed');
+        }
+        if (!in_array('sha256', hash_algos())) {
+            throw new PusherException('SHA256 appears to be unsupported - make sure you have support for it, or upgrade your version of PHP.');
+        }
+    }
+
+    public function trigger($channels, $event, $data, $alreadyEncoded = false)
+    {
+        if (is_string($channels)) {
+            $channels = array($channels);
+        }
+        $this->validateChannels($channels);
+        $dataEncoded = $alreadyEncoded ? $data : json_encode($data);
+        $client = new \GuzzleHttp\Client();
+        # TODO check channels array etc...
+        # TODO multi channel in one post ?
+        foreach ($channels as $channel) {
+            $postParams = array();
+            $postParams['name'] = $event;
+            $postParams['data'] = $dataEncoded;
+            $postParams['channel'] = '/'.$this->app.$channel;
+            $jsonBody = json_encode($postParams);
+            $bodyMd5 = md5($jsonBody);
+            $path = '/ws/'.$this->app.'/events';
+
+            $queryString = $this->buildQueryString($this->key, $this->secret, 'POST', $path, $timestamp = null);
+            $url = $this->url.$path.'?hash='.$bodyMd5.'&'.$queryString;
+            $response = $client->request(
+                'POST',
+                $url,
+                [
+                    'json' => [
+                        'name' => $event,
+                        'data' => $dataEncoded,
+                        'channel' => '/'.$this->app.$channel,
+                    ],
+                ]
+            );
+        }
+
+        if ($response->getStatusCode() === 200) {
+            echo 'OK';
+
+            return true;
+        } else {
+            echo 'NOK';
+
+            return false;
+        }
+    }
+
+    public function getChannels()
+    {
+    }
+
+    /**
+     * validate number of channels and channel name format.
+     */
+    private function validateChannels($channels)
+    {
+        if (count($channels) > 100) {
+            throw new PusherException('An event can be triggered on a maximum of 100 channels in a single call.');
+        }
+        foreach ($channels as $channel) {
+            $this->validateChannel($channel);
+        }
+    }
+
+    /**
+     * Ensure a channel name is valid based on our spec.
+     */
+    private function validateChannel($channel)
+    {
+        /*if (!preg_match('/\A[-a-zA-Z0-9_=@,.;]+\z/', $channel)) {
+            throw new PusherException('Invalid channel name ' . $channel);
+        }*/
+    }
+
+    private function buildQueryString($key, $secret, $method, $path, $timestamp = null)
+    {
+        $params = array();
+        $params['key'] = $key;
+        $params['timestamp'] = (is_null($timestamp) ? time() : $timestamp);
+        ksort($params);
+        $method = strtoupper($method);
+        $stringToSign = "$method\n".$path."\n".$this->arrayImplode('=', '&', $params);
+        echo($stringToSign);
+        $signature = hash_hmac('sha256', $stringToSign, $secret, false);
+        $params['signature'] = $signature;
+        ksort($params);
+        $queryString = $this->arrayImplode('=', '&', $params);
+
+        return $queryString;
+    }
+
+    private function arrayImplode($glue, $separator, $array)
+    {
+        if (!is_array($array)) {
+            return $array;
+        }
+        $string = array();
+        foreach ($array as $key => $val) {
+            if (is_array($val)) {
+                $val = implode(',', $val);
+            }
+            $string[] = "{$key}{$glue}{$val}";
+        }
+
+        return implode($separator, $string);
+    }
+}
